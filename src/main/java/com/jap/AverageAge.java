01b5f91c717eb52437125031c08b7b3c1e149fe4
M V K <krishna.mugala9@gmail.com>
2022-09-01 13:22:12 +0530

    added the logic inside the class

--- src/main/java/com/jap/AverageAge.java
+++ src/main/java/com/jap/AverageAge.java
@@ -1,22 +1,33 @@
 package com.jap;
 
+import java.util.Arrays;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.String.valueOf;
+
 public class AverageAge {
 
-    public String averageAgeCalculator(String[] ageRawData)
-    {
+    // This is a method that takes an array of strings and returns the average of the numbers in the array.
+    public <ageRawData> String averageAgeCalculator(String[] ageRawData) {
         double avg = 0;
-
-        try{
+        double total;
+        try {
             //Write the logic
 
+            for (String ageRawDatum : ageRawData) {
+                avg += parseDouble(ageRawDatum);
+                // finalAvg = avg/ageRawData.length;
+            }
+            total = avg / (double) ageRawData.length;
+            System.out.println(total);
+
+
             //Handle specific exception
-        }catch (Exception exception)
-        {
+        } catch (NumberFormatException exception) {
             return exception.toString();
         }
-        return String.valueOf(avg);
+        return valueOf(total);
     }
-
 
 
 }
--- src/test/java/com/jap/AverageAgeTest.java
+++ src/test/java/com/jap/AverageAgeTest.java
@@ -10,24 +10,23 @@ public class AverageAgeTest {
     private AverageAge age;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         age = new AverageAge();
     }
 
     @After
-    public void tearDown(){
+    public void tearDown() {
         age = null;
     }
 
     @Test
-    public void givenAStringArrayWithNumbersAsAgeFindAverageAgeAsResult()
-    {
-        assertEquals("34.25",age.averageAgeCalculator(new String[]{"35","26","32","44"}));
+    public void givenAStringArrayWithNumbersAsAgeFindAverageAgeAsResult() {
+        assertEquals("34.25", age.averageAgeCalculator(new String[]{"35", "26", "32", "44"}));
     }
+
     @Test
-    public void givenAStringArrayWithStringAndNumbersFindAverageAgeAsResult()
-    {
-        assertEquals("java.lang.NumberFormatException: For input string: \"thirty two\"",age.averageAgeCalculator(new String[]{"35","26","thirty two","44"}));
+    public void givenAStringArrayWithStringAndNumbersFindAverageAgeAsResult() {
+        assertEquals("java.lang.NumberFormatException: For input string: \"thirty two\"", age.averageAgeCalculator(new String[]{"35", "26", "thirty two", "44"}));
     }
 
 }
